29c103e2393fa07a9089dab7a926111fa5487b62
AlexanderMeshchaninov <alexander_freeman85@mail.ru>
2024-03-05 20:17:08 +0300

    Project updated 05.03.24

--- Outliers_lib/find_outliers.py
+++ Outliers_lib/find_outliers.py
@@ -61,3 +61,27 @@ def find_outliers_iqr(data, feature, left=1.5, right=1.5, log_scale=False):
     cleaned = data[(x >= lower_bound) & (x <= upper_bound)]
     
     return outliers, cleaned
+
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """
+    Классический метод (без модификаций), находит выбросы в данных, используя метод межквартильного размаха 
+    в левую и в правую стороны.
+    
+    Args:
+        data (pandas.DataFrame): набор данных
+        feature (str): имя признака, на основе которого происходит поиск выбросов
+        left (float, optional): количество межквартильных размахов в левую сторону распределения. По умолчанию 0.01.
+        right (float, optional): количество межквартильных размахов в правую сторону распределения. По умолчанию 0.99.
+
+    Returns:
+        pandas.DataFrame: наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame: очищенные данные, из которых исключены выбросы
+    """
+    x = data[feature]
+    
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    
+    return outliers, cleaned
